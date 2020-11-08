4c40e62549b796ab365438915f6cd5906a99f6a5
savannahblood <savannah@speedyquick.net>
2020-11-07 19:54:50 -0700

    create project

---
+++ cs260-cp3/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <header>
+      <router-link to="/">
+        <div id='logo'>
+          <img src="./assets/images/logo.png">
+        </div>
+      </router-link>
+      <a id='hamburger' data-toggle="collapse" href="#header-nav" role="button" aria-expanded="false" aria-controls="collapseExample">
+        <div class='line' id='one'></div>
+        <div class='line' id='two'></div>
+        <div class='line' id='three'></div>
+      </a>
+      <nav id='header-nav' class='collapse'>
+        <ul>
+          <router-link to="/pricing">
+            <li class="menu-item pricing"><a href="#">Pricing</a></li>
+          </router-link>
+          <router-link to="/holidays">
+            <li class="menu-item holidays"><a href="#">Holidays</a></li>
+          </router-link>
+          <router-link to="/signup">
+            <li class="menu-item signup"><a href="#">Sign Up</a></li>
+          </router-link>
+          <router-link to="/login">
+            <li class="menu-item login"><a href="#">Log In</a></li>
+          </router-link>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ cs260-cp3/src/views/Signup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <section id='signup'>
+      <form class='account-form'>
+        <h2>Sign Up</h2>
+        <div class='flex-input thirds'>
+          <input type='text' placeholder='First Name' id='first-name' />
+          <input type='text' placeholder='Last Name' id='last-name' />
+          <input type='text' placeholder='Username' id='username' />
+        </div>
+        <div class='flex-input halves'>
+          <input type='email' placeholder='Email' id='email' />
+          <input type='text' placeholder='Phone Number' id='phone' />
+        </div>
+        <div class='flex-input halves'>
+          <input type='password' placeholder='Password' id='password' />
+          <input type='password' placeholder='Confirm Password' id='confirm-password' />
+        </div>
+        <input type='submit' value='Sign Up' class='submit' />
+      </form>
+    </section>
+    <section id='contact'>
+      <form>
+        <h2>Questions?</h2>
+        <h3>We'll get right back to you.</h3>
+        <div class='flex-input halves'>
+          <input type='text' placeholder='Name' id='name' />
+          <input type='email' placeholder='Email' id='email'/>
+        </div>
+        <textarea placeholder='Message' id='message'></textarea>
+        <input type='submit' value='Send' class='submit' />
+      </form>
+    </section>
+  </div>
+</template>
